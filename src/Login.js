87abf7bd2bec791073308da9efaea60710fe3691
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2021-04-22 20:11:15 -0400

    intial code

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+class LogIn extends Component {
+  constructor () {
+    super()
+    this.state = {
+      user: {
+        userName: '',
+        password: ''
+      },
+      redirect: false
+    }
+  }
+
+  handleChange = (e) => {
+    const updatedUser = {...this.state.user}
+    const inputField = e.target.name
+    const inputValue = e.target.value
+    updatedUser[inputField] = inputValue
+
+    this.setState({user: updatedUser})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.mockLogIn(this.state.user)
+    this.setState({redirect: true})
+  }
+
+  render () {
+    if (this.state.redirect) {
+      return (<Redirect to="/userProfile"/>)
+    }
