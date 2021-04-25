fc10b319280756e0d39775016a4ffa0d55c7d71c
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2021-04-24 20:01:16 -0400

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
+import LogIn from './Login.js'
 
 class App extends Component {
 
@@ -17,18 +18,26 @@ class App extends Component {
     }
   }
 
+  mockLogIn = (logInInfo) => {
+   const newUser = {...this.state.currentUser}
+   newUser.userName = logInInfo.userName
+   this.setState({currentUser: newUser})
+ }
+
   render() {
 
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
     const UserProfileComponent = () => (
         <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
     );
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
 
     return (
         <Router>
           <div>
-            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/Home" render={HomeComponent}/>
             <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/Login" render={LogInComponent}/>
           </div>
         </Router>
     );
